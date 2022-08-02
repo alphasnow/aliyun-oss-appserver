a56df1374e3d14720f2744bc5401c0726169c034
weifeng <wind91@foxmail.com>
2022-08-02 13:09:36 +0800

    fix: examples callback

--- examples/callback.php
+++ examples/callback.php
@@ -2,9 +2,9 @@
 require "./vendor/autoload.php";
 
 use AlphaSnow\OSS\AppServer\Callback;
-use AlphaSnow\OSS\AppServer\ServerCallback;
+use AlphaSnow\OSS\AppServer\Callbacks\ServerCallback;
 
-$status = (new ServerCallback(new Callback))->verifyByRequest();
+$status = (new ServerCallback(new Callback))->verify();
 if ($status == false) {
     header("HTTP/1.1 403 Forbidden");
     header("Content-Type: application/json; charset=utf-8");
